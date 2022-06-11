6ada161d93233bf329e2b4f989004b307229752d
Andrew Casta <acasta4704@gmail.com>
2022-06-11 23:12:37 +1000

    Add class for ragdoll and collision
    Add collision to find closest rb & add force
    
    On death/transition to ragdoll, force is added to specific limb when hit
    
    Applied to prefab

--- Assets/Scripts/CopMovement.cs
+++ Assets/Scripts/CopMovement.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class CopMovement : MonoBehaviour
 {
-    Rigidbody copRb;
-    Rigidbody[] ragdollRB;
-    Collider[] ragdollColliders;
     [SerializeField] bool isAlive;
     [SerializeField] float moveSpeed;
     [SerializeField] float turnSpeed;
-    Animator animator;
 
     [SerializeField] float distanceToShoot;
     [SerializeField] bool readyToShoot;
@@ -19,41 +15,29 @@ public class CopMovement : MonoBehaviour
     [SerializeField] GameObject bullet;
     [SerializeField] float bulletForce;
 
-
-    [SerializeField] float bulletCollisionForce;
-
     GameObject copBulletSpawnObj;
 
     float distanceToPlayer;
     GameObject player;
     Vector3 directionToPlayer;
 
-    SpawnManager spawnManager;
     GameManager gameManager;
     float[] gameBounds;
 
+    Animator animator;
     AudioSource playerAudio;
     [SerializeField] AudioClip gunshotSound;
-
-    private void Awake()
-    {
-        ragdollRB = GetComponentsInChildren<Rigidbody>();
-        ragdollColliders = GetComponentsInChildren<Collider>();
-        copRb = GetComponent<Rigidbody>();
-        DisableRagdoll();
-    }
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         isAlive = true;
-        spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        animator = GetComponent<Animator>();
         gameBounds = gameManager.GetBoundry();
         copBulletSpawnObj = transform.Find("Bullet Point").gameObject;
         playerAudio = GetComponent<AudioSource>();
-        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -77,9 +61,6 @@ public class CopMovement : MonoBehaviour
         {
             isAlive = false;
             gameManager.copCount--;
-            EnableRagdoll();
-            Vector3 direction = (collision.GetContact(0).point - transform.position).normalized;
-            copRb.AddForce(-direction * bulletCollisionForce, ForceMode.Impulse);
         }
     }
 
@@ -123,36 +104,5 @@ public class CopMovement : MonoBehaviour
         playerAudio.PlayOneShot(gunshotSound);
         Instantiate(bullet, copBulletSpawnObj.transform.position, copBulletSpawnObj.transform.rotation).GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * bulletForce, ForceMode.Impulse);
         reloadTimer = 0;
-    }
-
-    void DisableRagdoll()
-    {
-        foreach (Rigidbody rb in ragdollRB)
-        {
-            rb.isKinematic = true;
-        }
-        copRb.isKinematic = false;
-        foreach (Collider col in ragdollColliders)
-        {
-            col.enabled = false;
-        }
-        GetComponent<Collider>().enabled = true;
-    }
-
-    void EnableRagdoll()
-    {
-        animator.enabled = false;
-        foreach (Rigidbody rb in ragdollRB)
-        {
-            rb.isKinematic = false;
-        }
-        copRb.isKinematic = true;
-
-        foreach (Collider col in ragdollColliders)
-        {
-            col.enabled = true;
-        }
-        GetComponent<Collider>().enabled = false;
-
     }
 }
---
+++ Assets/Scripts/RagdollBulletCollision.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollBulletCollision : MonoBehaviour
+{
+    Rigidbody mainRb;
+    Rigidbody[] ragdollRB;
+    Animator animator;
+
+    [SerializeField] float bulletCollisionForce;
+
+    private void Awake()
+    {
+        ragdollRB = GetComponentsInChildren<Rigidbody>();
+        mainRb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        DisableRagdoll();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("bullet"))
+        {
+            // Find closest ragdoll collider
+            EnableRagdoll();
+            Collider[] hitColliders = Physics.OverlapSphere(collision.gameObject.transform.position, 2f);
+            Collider bulletCollider = collision.gameObject.GetComponent<Collider>();
+            Collider closestHitbox = null;
+            float closestHitboxDistance = 10f;
+            foreach (Collider hitbox in hitColliders)
+            {
+                if (hitbox != bulletCollider)
+                {
+                    float hitboxDistance = (bulletCollider.gameObject.transform.position - hitbox.gameObject.transform.position).sqrMagnitude;
+                    if (hitboxDistance < closestHitboxDistance)
+                    {
+                        closestHitboxDistance = hitboxDistance;
+                        closestHitbox = hitbox;
+                    }
+                    // Debug.Log($"Hitbox: {hitbox.gameObject.name} - Distance: {hitboxDistance}");
+                }
+            }
+            // Apply force
+            Debug.Log($"Closet Hitbox: {closestHitbox.gameObject.name}");
+            Vector3 direction = (closestHitbox.gameObject.transform.position - collision.gameObject.transform.position).normalized;
+            closestHitbox.gameObject.GetComponent<Rigidbody>().AddForce(-direction * bulletCollisionForce, ForceMode.Impulse);
+
+        }
+    }
+
+    void DisableRagdoll()
+    {
+        foreach (Rigidbody rb in ragdollRB)
+        {
+            rb.isKinematic = true;
+            rb.detectCollisions = false;
+        }
+        mainRb.isKinematic = false;
+        mainRb.detectCollisions = true;
+        animator.enabled = true;
+    }
+
+    void EnableRagdoll()
+    {
+        foreach (Rigidbody rb in ragdollRB)
+        {
+            rb.isKinematic = false;
+            rb.detectCollisions = true;
+        }
+        mainRb.isKinematic = true;
+        mainRb.detectCollisions = false;
+        animator.enabled = false;
+    }
+}
